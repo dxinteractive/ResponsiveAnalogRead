76c0ebe8e0edf3c5bc15be1c7d371b0cd9f32bc9
Damien Clarke <dxinteractive@gmail.com>
2018-07-12 21:43:33 +1000

    Add ability to have exponentional sliders

--- docs/src/util/ParcelModifiers.js
+++ docs/src/util/ParcelModifiers.js
@@ -1,9 +1,15 @@
 // @flow
 import type Parcel, {ChangeRequest} from 'parcels-react';
 
-export const changeToNumber = parcel => parcel.modifyChange((parcel: Parcel, changeRequest: ChangeRequest) => {
-    let value: number = Number(changeRequest.data().value);
-    if(!isNaN(value)) {
-        parcel.onChange(value);
-    }
-});
+export const numberToString = () => parcel => parcel
+    .modifyValue(value => `${value}`)
+    .modifyChange((parcel: Parcel, changeRequest: ChangeRequest) => {
+        let value: number = Number(changeRequest.data().value);
+        if(!isNaN(value)) {
+            parcel.onChange(value);
+        }
+    });
+
+export const numberToExp = (pow: number) => parcel => parcel
+    .modifyValue(value => Math.pow(value, 1/pow))
+    .modifyChangeValue(value => Math.pow(value, pow));
