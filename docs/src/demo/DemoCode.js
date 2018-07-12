d0facfd8fb75bb8794bb57bde00884bd287f04a3
Damien Clarke <dxinteractive@gmail.com>
2018-07-12 21:41:44 +1000

    Render demo code

--- docs/src/demo/DemoCode.js
+++ docs/src/demo/DemoCode.js
@@ -1,23 +1,73 @@
 // @flow
 
-const DEMO_CODE = `#include <Arduino.h>
+let render = ({instanciations, setup, reads, values}) => `#include <Arduino.h>
 #include <ResponsiveAnalogRead.h>
 
-ResponsiveAnalogRead analog1;
-analog1.pin(A0);
-analog1.smooth(10);
-analog1.quick();
-analog1.settle();
+${instanciations}
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);${setup}
 }
 
 void loop() {
-  analog1.update();
-  Serial.println(analog.value());
+${reads}
+
+${values}
+
   // more code here...
+  // optional delay
   delay(10);
 }`;
 
-export default () => DEMO_CODE;
+type Props = {
+    amount: number,
+    noisefloor: number,
+    smooth: boolean,
+    quick: boolean,
+    settle: boolean,
+    pin: boolean
+};
+
+export default ({amount, noisefloor, smooth, quick, settle, pin}: Props): string => {
+    let instances = Array(amount || 0)
+        .fill(null)
+        .map((u, index) => index + 1);
+
+    let instanciations = instances
+        .map(key => `ResponsiveAnalogRead analog${key};`)
+        .join("\n");
+
+    let renderSetup = (key) => [
+        pin && `analog${key}.pin(A${key})`,
+        noisefloor && `analog${key}.noisefloor(${noisefloor});`,
+        smooth && `analog${key}.smooth();`,
+        quick && `analog${key}.quick();`,
+        settle && `analog${key}.settle();`
+    ]
+        .filter(ii => ii)
+        .map(ii => "  " + ii)
+        .join("\n");
+
+    let setup = instances
+        .map(renderSetup)
+        .join("\n\n");
+
+    if(setup) {
+        setup = `\n\n${setup}`;
+    }
+
+    let reads = instances
+        .map(key => `  analog${key}.read();`)
+        .join("\n");
+
+    let values = instances
+        .map(key => `  Serial.print("Analog${key} value: ");\n  Serial.println(analog${key}.value());`)
+        .join("\n");
+
+    return render({
+        instanciations,
+        setup,
+        reads,
+        values
+    });
+};
